ca38fc7de62522b566261ebf559eeda44a9e2df6
Soaring Radio <burroughszeb@me.com>
2016-09-16 14:51:36 -0500

    Update #3.3.4
    Usernames must be unique.

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -50,7 +50,7 @@ class AuthController extends Controller
     {
         return Validator::make($data, [
             'name' => 'required|max:255|regex:/^[0-9a-zA-Zs\s\-\_]*$/',
-            'username' => 'required|max:255|alpha_dash',
+            'username' => 'required|unique:users|max:255|alpha_dash',
             'email' => 'required|email|max:255|unique:users',
             'password' => 'required|min:6|confirmed',
             'phone' => 'required|regex:/^[+(0-9)]{1,6}[0-9()\s-]*$/|min:10|max:25',
