7cd24c12ea4e375ddcea56b9460bbac83886873d
Sys <frameworkthegreat@gmail.com>
2016-10-09 11:06:40 +0100

    Working community base plate

--- app/Http/Controllers/CommunityController.php
+++ app/Http/Controllers/CommunityController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Community;
+use App\User;
 
 use App\Http\Requests;
 
@@ -11,12 +12,13 @@ use App\Http\Requests;
 class CommunityController extends Controller
 {
     public function getCommunity($url){
-
       $getCommunity = Community::where('url', '=', $url)->first();
       if($getCommunity == ''){ //If a community was searched but doesn't exist a 404 will be thrown ,(Literally).
         abort(404);
       }else{
-        return view('communities.community');
+        $owner = User::where('id', '=', $getCommunity->owner)->first();
+        //return $owner;
+        return view('communities.community', compact('owner'));
         //return $getCommunity;
       }
 
