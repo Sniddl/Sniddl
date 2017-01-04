7fb08000241f5d3d4c893cab0867d89e44541fe0
Soaring Radio <burroughszeb@me.com>
2017-01-04 12:37:18 -0600

    after watch tut

--- socket.js
+++ socket.js
@@ -1,22 +1,10 @@
 #!/usr/bin/env nodejs
 
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error socket.js line 8');}
-    res.writeHead(200);
-    res.end(data);});}
-
-
-var server = require('http').createServer(handler);
-var Socket = require('socket.io');
-var io = new Socket();
-var fs = require('fs');
+var app   = require('express')();
+var http  = require('http').Server(app);
+var io    = require('socket.io')(http);
 var Redis = require('ioredis');
 var redis = new Redis();
-io.set('origins', '*:*');
 
 redis.psubscribe('*', function(err, count) {});
 
@@ -26,6 +14,9 @@ redis.on('pmessage', function(subscribed, channel, message) {
     io.emit(channel + ':' + message.event, message.data);
 });
 
-server.listen(3000,function(){
+
+
+
+http.listen(3000,function(){
   console.log("listening to port 3000");
 });
