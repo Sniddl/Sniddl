013252986860f9a5d2b45cdeeed843cdef6c5d9e
Soaring Radio <burroughszeb@me.com>
2017-01-03 18:32:02 -0600

    a

--- socket.js
+++ socket.js
@@ -1,21 +1,30 @@
 #!/usr/bin/env nodejs
-var server = require('http').createServer(function (req, res) {
+var http = require('http');
+http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
 }).listen(8080, 'localhost');
+console.log('Server running at http://localhost:8080/');
 
-var io = require('socket.io')(server);
-var Redis = require('ioredis');
-var redis = new Redis();
 
-redis.psubscribe('*', function(err, count) {});
-
-redis.on('pmessage', function(subscribed, channel, message) {
-    console.log(channel + ':' + message.event, message.data);
-    message = JSON.parse(message);
-    io.emit(channel + ':' + message.event, message.data);
-});
-
-// server.listen(3000,function(){
-//   console.log("listening to port 3000");
+// #!/usr/bin/env nodejs
+// var server = require('http').createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.end('Hello World\n');
+// }).listen(8080, 'localhost');
+//
+// var io = require('socket.io')(server);
+// var Redis = require('ioredis');
+// var redis = new Redis();
+//
+// redis.psubscribe('*', function(err, count) {});
+//
+// redis.on('pmessage', function(subscribed, channel, message) {
+//     console.log(channel + ':' + message.event, message.data);
+//     message = JSON.parse(message);
+//     io.emit(channel + ':' + message.event, message.data);
 // });
+//
+// // server.listen(3000,function(){
+// //   console.log("listening to port 3000");
+// // });
