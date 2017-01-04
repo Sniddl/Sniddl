d2d10345926951a08f7e4f222d9ea69a1fecaf82
Soaring Radio <burroughszeb@me.com>
2017-01-03 18:24:07 -0600

    maybe if I add a bin script

--- socket.js
+++ socket.js
@@ -1,61 +1,21 @@
-// var server = require('http').createServer(function (req, res) {
-//   res.writeHead(200, {'Content-Type': 'text/plain'});
-//   res.end('Hello World\n');
-// });
-//
-// var io = require('socket.io')(server);
-// var Redis = require('ioredis');
-// var redis = new Redis();
-// redis.psubscribe('*', function(err, count) {});
-//
-// redis.on('pmessage', function(subscribed, channel, message) {
-//     console.log(channel + ':' + message.event, message.data);
-//     message = JSON.parse(message);
-//     io.emit(channel + ':' + message.event, message.data);
-// });
-//
-// server.listen(3000,function(){
-//   console.log("listening to port 3000");
-// });
-//
-//
-
-
-
-
-var app = require('http').createServer(handler)
-var io = require('socket.io')(app);
-var fs = require('fs');
-
-app.listen(3000);
-
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-
-    res.writeHead(200);
-    res.end(data);
-  });
-}
-
-io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+#!/usr/bin/env nodejs
+var server = require('http').createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
 });
 
-
+var io = require('socket.io')(server);
 var Redis = require('ioredis');
 var redis = new Redis();
+
 redis.psubscribe('*', function(err, count) {});
 
 redis.on('pmessage', function(subscribed, channel, message) {
     console.log(channel + ':' + message.event, message.data);
     message = JSON.parse(message);
     io.emit(channel + ':' + message.event, message.data);
+});
+
+server.listen(3000,function(){
+  console.log("listening to port 3000");
 });
