ae097eabe1ea899ba81a0e48aa4d5294c4924a50
Soaring Radio <burroughszeb@me.com>
2017-01-03 18:27:37 -0600

    pls

--- socket.js
+++ socket.js
@@ -2,7 +2,7 @@
 var server = require('http').createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-});
+}).listen(8080, 'localhost');
 
 var io = require('socket.io')(server);
 var Redis = require('ioredis');
@@ -16,6 +16,6 @@ redis.on('pmessage', function(subscribed, channel, message) {
     io.emit(channel + ':' + message.event, message.data);
 });
 
-server.listen(3000,function(){
-  console.log("listening to port 3000");
-});
+// server.listen(3000,function(){
+//   console.log("listening to port 3000");
+// });
