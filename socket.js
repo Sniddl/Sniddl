66a01fd433379bfed6b8ba5fb8f6830b4a6cbf02
Soaring Radio <burroughszeb@me.com>
2017-01-05 23:44:59 -0600

    ??

--- socket.js
+++ socket.js
@@ -1,9 +1,25 @@
 #!/usr/bin/env nodejs
 
-var server = require('http').Server();
-var io = require('socket.io')(server);
+var app = require('http').createServer(handler);
+var io = require('socket.io')(app);
+var fs = require('fs');
 var Redis = require('ioredis');
 var redis = new Redis();
+
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
 
 redis.psubscribe('*', function(err, count) {});
 
@@ -13,4 +29,4 @@ redis.on('pmessage', function(subscribed, channel, message) {
     io.emit(channel + ':' + message.event, message.data);
 });
 
-server.listen(3000, 'http://sniddl.com');
+app.listen(3000);
