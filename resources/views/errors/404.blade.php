123e5e157561e33849e705138ceec526bced1e17
Soaring Radio <burroughszeb@me.com>
2017-02-11 13:54:46 -0600

    Assets + Error Pages

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,25 @@
+
+<style media="screen">
+
+  .error-img-wrapper {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    height: 75vh;
+    width: 75vw;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none;
+    -o-user-select: none;
+    user-select: none;
+    background-image: url('/img/404.png');
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+}
+</style>
+
+<div class="error-img-wrapper">
+
+</div>
